9ddd9c602667094a7645c9ea915fa7f97a02c058
Sjantos <sebastian.jantos@hotmail.com>
2017-12-18 12:59:20 +0100

    Project in VS2017

---
+++ OpenGLProject2017/OpenGLProject/OpenGLProject/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace OpenGLProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenGL.GlControl();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.glControl.ColorBits = ((uint)(24u));
+            this.glControl.DepthBits = ((uint)(0u));
+            this.glControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.glControl.Location = new System.Drawing.Point(0, 0);
+            this.glControl.MultisampleBits = ((uint)(0u));
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(784, 561);
+            this.glControl.StencilBits = ((uint)(0u));
+            this.glControl.TabIndex = 0;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelMenu.Location = new System.Drawing.Point(584, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 561);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenGL.GlControl glControl;
+        private System.Windows.Forms.Panel panelMenu;
+    }
+}
+
